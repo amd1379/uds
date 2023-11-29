b3b6271e7231570c0f03e77ca0db8c749dc20ece
amd1379 <152440714+amd1379@users.noreply.github.com>
2023-11-29 23:45:33 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,11 +11,6 @@ read -p "(Default value: @123%): " secret
 if [[ -z "$secret" ]]; then
   secret="@123%"
 fi
-### add block list
-#cd /usr/local/x-ui/bin
-#wget https://github.com/bootmortis/iran-hosted-domains/releases/latest/download/iran.dat
-#wget https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat
-###
 
 apt update
 
@@ -37,7 +32,7 @@ mkdir /usr/local/user-detail-server -v
 # mkdir /usr/local/user-detail-server/static -v
 cd /usr/local/user-detail-server
 
-wget https://raw.githubusercontent.com/ahmady2001/UserDetailServer/master/UserDetailServer.py -O UserDetailServer.py
+wget https://raw.githubusercontent.com/amd1379/uds/master/UserDetailServer.py -O UserDetailServer.py
 
 #### make config.ini
 cat << EOF > config.ini
@@ -46,20 +41,9 @@ secret = $secret
 EOF
 echo "Configuration saved to config.txt"
 
-# cd /usr/local/user-detail-server/templates
-
-# wget https://raw.githubusercontent.com/ahmady2001/UserDetailServer/master/templates/index.html -O index.html
-
-# cd /usr/local/user-detail-server/static
-
-# wget https://raw.githubusercontent.com/ahmady2001/UserDetailServer/master/static/style.min.css -O style.min.css
-# wget https://raw.githubusercontent.com/ahmady2001/UserDetailServer/master/static/script.min.js -O script.min.js
-# wget https://raw.githubusercontent.com/ahmady2001/UserDetailServer/master/static/chart.min.js -O chart.min.js
-# wget https://raw.githubusercontent.com/ahmady2001/UserDetailServer/master/static/Sora-VariableFont_wght.ttf -O Sora-VariableFont_wght.ttf
-
 cd /etc/systemd/system
 
-wget https://raw.githubusercontent.com/ahmady2001/UserDetailServer/master/user-detail-server.service -O user-detail-server.service
+wget https://raw.githubusercontent.com/amd1379/uds/master/user-detail-server.service -O user-detail-server.service
 
 systemctl enable user-detail-server
 systemctl start user-detail-server
